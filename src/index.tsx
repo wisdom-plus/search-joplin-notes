bc3c0095e969ec18a0474899435d8b5c8678881c
tomoron <tomoron326@outlook.jp>
2022-07-07 12:27:39 +0900

    add:searchText

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,25 @@
 import { ActionPanel, Detail, List, Action } from "@raycast/api";
+import { useState } from "react";
 
 export default function Command() {
+  const [searchText, setSearchText] = useState("");
+
   return (
-    <List>
-      <List.Item
-        icon="list-icon.png"
-        title="Greeting"
-        actions={
-          <ActionPanel>
-            <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
-          </ActionPanel>
-        }
-      />
+    <List searchBarPlaceholder="Search keywords" onSearchTextChange={setSearchText}>
+      {searchText && (
+        <>
+          <List.Item
+            icon="list-icon.png"
+            title="Greeting"
+            actions={
+              <ActionPanel>
+                <Action.Push title="Show Details" target={<Detail markdown="# Hey! 👋" />} />
+              </ActionPanel>
+            }
+          />
+          <List.Item icon="list-icon.png" title={searchText} />
+        </>
+      )}
     </List>
   );
 }
