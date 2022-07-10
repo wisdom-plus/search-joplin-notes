425eecd2296637ff44dc9bf29485d75d9174d450
tomoron <tomoron326@outlook.jp>
2022-07-10 22:20:52 +0900

    add:constnts.ts

---
+++ src/utils/constants.ts
@@ -0,0 +1,6 @@
+import { getPreferenceValues } from "@raycast/api";
+
+export const API_URL = (keyword: string): string =>
+  `http://localhost:41184/search?query=${keyword}*&fields=id,title,body&token=${API_Token}`;
+
+export const API_Token = getPreferenceValues().joplin_token;
