8b80ad26d1812f204afbc4015b27656c6cfbee5e
tomoron <tomoron326@outlook.jp>
2022-07-12 16:13:16 +0900

    add:fix:props

--- src/components/NotesList.tsx
+++ src/components/NotesList.tsx
@@ -1,11 +1,12 @@
 import { ActionPanel, List, Action, Detail } from "@raycast/api";
 import type { data } from "../utils/types";
 
-export const NotesList = (data: data, path: string) => {
+type Props = { data: data; path: string };
+
+export const NotesList = ({ data, path }: Props) => (
   <List.Item
     icon="list-icon.png"
     title={data.title}
-    key={data.id}
     actions={
       <ActionPanel>
         <Action.Push
@@ -23,5 +24,5 @@ export const NotesList = (data: data, path: string) => {
         />
       </ActionPanel>
     }
-  />;
-};
+  />
+);
