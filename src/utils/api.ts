db02001941bf8087da6d9fd83a32820da8406b72
tomoron <tomoron326@outlook.jp>
2022-07-13 12:45:09 +0900

    rename:type

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,10 +1,10 @@
 import fetch from "node-fetch";
 import { API_URL } from "./constants";
-import type { joplindata } from "./types";
+import type { notedata } from "./types";
 
 export const fetchnotes = async (keyword: string) => {
   const response = await fetch(API_URL(keyword), { method: "GET" });
   if (response.ok) {
-    return response.json() as Promise<joplindata>;
+    return response.json() as Promise<notedata>;
   }
 };
