dcd69030daae4e84460ebca272192eafde72d892
tomoron <tomoron326@outlook.jp>
2022-07-13 12:44:22 +0900

    fix:typo

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -7,4 +7,4 @@ export const API_Token = getPreferenceValues().joplin_token;
 
 export const RaycastBundleId = "com.raycast.macos";
 
-export const JopliBundleId = "net.cozic.joplin-desktop";
+export const JoplinBundleId = "net.cozic.joplin-desktop";
