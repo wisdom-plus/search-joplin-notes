1909fb1e736cd60a75557aab49cf122d4519e17d
tomoron <tomoron326@outlook.jp>
2022-07-13 12:23:42 +0900

    add:joplinbundleid

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -5,4 +5,6 @@ export const API_URL = (keyword: string): string =>
 
 export const API_Token = getPreferenceValues().joplin_token;
 
-export const JoplinBundleId = "com.raycast.macos";
+export const RaycastBundleId = "com.raycast.macos";
+
+export const JopliBundleId = "net.cozic.joplin-desktop";
