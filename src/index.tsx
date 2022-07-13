273364bec788c47f19183e0d4e3a8b8ab7f19967
tomoron <tomoron326@outlook.jp>
2022-07-13 13:23:42 +0900

    fix:letter case

--- src/index.tsx
+++ src/index.tsx
@@ -2,20 +2,19 @@ import { List } from "@raycast/api";
 import { useState, useEffect } from "react";
 import { NotesList } from "./components/NotesList";
 import { fetchnotes } from "./utils/api";
-import { notedata } from "./utils/types";
+import { NoteData } from "./utils/types";
 import { useGetPath } from "./utils/usegetpath";
 
 export default function Command() {
   const [searchText, setSearchText] = useState("");
-  const [result, setResult] = useState<notedata>();
+  const [result, setResult] = useState<NoteData>();
 
   const path = useGetPath();
 
   useEffect(() => {
     if (searchText) {
-      fetchnotes(searchText).then((data) => {
-        setResult(() => data);
-      });
+      const notes = fetchnotes(searchText);
+      setResult(() => notes);
     }
   }, [searchText]);
 
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,2 +1,2 @@
 export type data = { id: string; title: string; body: string };
-export type notedata = { items: data[] };
+export type NoteData = { items: data[] };
