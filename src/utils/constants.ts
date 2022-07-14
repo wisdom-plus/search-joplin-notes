181817760a6d26ceafb33832d16bcf4e01463054
tomoron <tomoron326@outlook.jp>
2022-07-14 19:37:26 +0900

    pref:port

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,8 +1,10 @@
 import { getPreferenceValues } from "@raycast/api";
+import { pingjoplin } from "./api";
 
-export const API_URL = (keyword: string): string =>
-  `http://localhost:41184/search?query=${keyword}*&fields=id,title,body&token=${API_Token}`;
-
+export const API_URL = async (keyword: string): Promise<string> => {
+  const port = await pingjoplin();
+  return `http://localhost:${port}/search?query=${keyword}*&fields=id,title,body&token=${API_Token}`;
+};
 export const API_Token = getPreferenceValues().joplin_token;
 
 export const RaycastBundleId = "com.raycast.macos";
