2fadfef1ddf475953adf9da22898b273b80d8c8b
tomoron <tomoron326@outlook.jp>
2022-07-14 19:37:46 +0900

    add:pingjoplin

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,8 +2,9 @@ import fetch from "node-fetch";
 import { API_URL } from "./constants";
 import type { NoteData } from "./types";
 
-export const fetchnotes = (keyword: string) => {
-  const response = fetch(API_URL(keyword), { method: "GET" }).then((res) => {
+export const fetchnotes = async (keyword: string) => {
+  console.log(API_URL("test"));
+  const response = fetch(await API_URL(keyword), { method: "GET" }).then((res) => {
     if (res.ok) {
       return res.json() as Promise<NoteData>;
     } else {
@@ -11,4 +12,24 @@ export const fetchnotes = (keyword: string) => {
     }
   });
   return response;
+};
+
+export const pingjoplin = async () => {
+  const port = { port: 41184 };
+  for (let portToTest = 41183; portToTest <= 41194; portToTest++) {
+    const result = await fetch(`http://localhost:${portToTest}/ping`, { method: "GET" })
+      .then((res) => {
+        if (!res.ok && res.body === null) {
+          throw new Error("Error pinging Joplin");
+        }
+        return res.body?.read().toString();
+      })
+      .catch(() => false);
+
+    if (result == "JoplinClipperServer") {
+      port.port = portToTest;
+      break;
+    }
+  }
+  return port.port;
 };
